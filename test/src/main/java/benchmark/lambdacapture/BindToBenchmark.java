cd8af9806ff9e38f2e286fb3db46b7e95754189a
Wenlei Xie <wxie@fb.com>
2017-05-03 18:47:35 -0700

    Initial commit for lambda capture benchmark

---
+++ test/src/main/java/benchmark/lambdacapture/BindToBenchmark.java
@@ -0,0 +1,96 @@
+package benchmark.lambdacapture;
+
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.VerboseMode;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.util.concurrent.TimeUnit;
+
+
+@SuppressWarnings("MethodMayBeStatic")
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 10)
+@Fork(2)
+@Measurement(iterations = 10)
+@BenchmarkMode(Mode.Throughput)
+public class BindToBenchmark
+{
+    private static final int NUM_ROWS = 100_000;
+
+    @Benchmark
+    @OperationsPerInvocation(NUM_ROWS * 1)
+    public void invoke1()
+            throws Throwable
+    {
+        final int INVOKE_TIME = 1;
+
+        MethodHandle methodHandle = MethodHandles.lookup().unreflect(BindToBenchmark.class.getMethod("originalMethod", BindToBenchmark.class, Long.class, Long.class));
+        for (int i = 0; i < NUM_ROWS; i++) {
+            MethodHandle bindedMethodHandle = MethodHandles.insertArguments(methodHandle, 0, this, Long.valueOf(i));
+            for (int j = 0; j < INVOKE_TIME; j++) {
+                Long ret = (Long) bindedMethodHandle.invokeExact(Long.valueOf(j));
+            }
+        }
+    }
+
+
+    @Benchmark
+    @OperationsPerInvocation(NUM_ROWS * 10)
+    public void invoke10()
+            throws Throwable
+    {
+        final int INVOKE_TIME = 10;
+
+        MethodHandle methodHandle = MethodHandles.lookup().unreflect(BindToBenchmark.class.getMethod("originalMethod", BindToBenchmark.class, Long.class, Long.class));
+        for (int i = 0; i < NUM_ROWS; i++) {
+            MethodHandle bindedMethodHandle = MethodHandles.insertArguments(methodHandle, 0, this, Long.valueOf(i));
+            for (int j = 0; j < INVOKE_TIME; j++) {
+                Long ret = (Long) bindedMethodHandle.invokeExact(Long.valueOf(j));
+            }
+        }
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(NUM_ROWS * 100)
+    public void invoke100()
+            throws Throwable
+    {
+        final int INVOKE_TIME = 100;
+
+        MethodHandle methodHandle = MethodHandles.lookup().unreflect(BindToBenchmark.class.getMethod("originalMethod", BindToBenchmark.class, Long.class, Long.class));
+        for (int i = 0; i < NUM_ROWS; i++) {
+            MethodHandle bindedMethodHandle = MethodHandles.insertArguments(methodHandle, 0, this, Long.valueOf(i));
+            for (int j = 0; j < INVOKE_TIME; j++) {
+                Long ret = (Long) bindedMethodHandle.invokeExact(Long.valueOf(j));
+            }
+        }
+    }
+
+    public static void main(String[] args)
+            throws Throwable
+    {
+        // assure the benchmarks are valid before running
+        Options options = new OptionsBuilder()
+                .verbosity(VerboseMode.NORMAL)
+                .include(BindToBenchmark.class.getSimpleName())
+                .build();
+        new Runner(options).run();
+    }
+
+    public static Long originalMethod(BindToBenchmark instance, Long a, Long b) {
+        return a + b;
+    }
+}
